1e157919d48b68b16feb61a081b25ffe3a961bde
IgorDodukh <ihor.dodukh@gmail.com>
2016-09-21 00:47:33 +0300

    simple test finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
 /**
  * Created by Ihor on 9/21/2016. All rights reserved!
  */
 public class Main {
     public static void main(String[] args) {
+        By searchFieldLocator = By.xpath("//input[@class='b_searchbox']");
+        By searchResultsTitleLocator = By.cssSelector("ol li h2>a");
 
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.bing.com/");
+
+        WebElement searchField = driver.findElement(searchFieldLocator);
+        searchField.sendKeys("automation");
+        searchField.submit();
+
+        Wait<WebDriver> wait = new WebDriverWait(driver, 10).withMessage("Search results not found");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(searchResultsTitleLocator));
+
+        String pageTitle = driver.getTitle();
+        System.out.println("Page Title: " + pageTitle);
+
+        List<WebElement> allSearchResults = driver.findElements(searchResultsTitleLocator);
+
+        for(WebElement eachResult : allSearchResults) {
+            System.out.println("Title: " + eachResult.getText() + "\nLink: " + eachResult.getAttribute("href") + "\n");
+        }
+
+        driver.quit();
     }
 }
