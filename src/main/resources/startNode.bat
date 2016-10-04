bc458fa8def2cfeb8d0d4f0cfff36926c4a3b0f8
IgorDodukh <ihor.dodukh@gmail.com>
2016-10-04 23:53:52 +0300

    implement selenium grid

---
+++ src/main/resources/startNode.bat
@@ -0,0 +1,1 @@
+java -jar selenium-server-standalone-2.53.1.jar -role node -hub http://localhost:4444/grid/register -browser "browserName=firefox,maxInstances=3" -browser "browserName=chrome,maxInstances=2" -Dchrome.driver.executable="chrome.exe"
