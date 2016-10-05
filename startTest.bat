0e58fef0cf4f330f43eb9d46cac6da3f0d93c895
IgorDodukh <ihor.dodukh@gmail.com>
2016-10-06 00:31:19 +0300

    adding executable scripts

--- startTest.bat
+++ startTest.bat
@@ -1,6 +1,6 @@
-call mvn -Dbrowser="firefox" surefire-report:report -DreportsDirectory=${basedir}${file.separator}target${file.separator}site  test
-call mvn -Dbrowser="chrome" surefire-report:report -DreportsDirectory=${basedir}${file.separator}target${file.separator}site test
-call mvn -Dbrowser="ie" surefire-report:report -DreportsDirectory=${basedir}${file.separator}target${file.separator}site test
-call mvn -Dbrowser="phantomjs" surefire-report:report -DreportsDirectory=${basedir}${file.separator}target${file.separator}site test
+call mvn -Dbrowser="firefox" surefire-report:report -DreportsPath=${basedir}${file.separator}target${file.separator}site  test
+call mvn -Dbrowser="chrome" surefire-report:report -DreportsPath=${basedir}${file.separator}target${file.separator}site test
+call mvn -Dbrowser="ie" surefire-report:report -DreportsPath=${basedir}${file.separator}target${file.separator}site test
+call mvn -Dbrowser="phantomjs" surefire-report:report -DreportsPath=${basedir}${file.separator}target${file.separator}site test
 
 pause
--- startTest.sh
+++ startTest.sh
@@ -1,6 +1,6 @@
-mvn -Dbrowser="firefox" surefire-report:report -DreportsDirectory=${basedir}${file.separator}target${file.separator}site  test
-mvn -Dbrowser="chrome" surefire-report:report -DreportsDirectory=${basedir}${file.separator}target${file.separator}site test
-mvn -Dbrowser="ie" surefire-report:report -DreportsDirectory=${basedir}${file.separator}target${file.separator}site test
-mvn -Dbrowser="phantomjs" surefire-report:report -DreportsDirectory=${basedir}${file.separator}target${file.separator}site test
+mvn -Dbrowser="firefox" surefire-report:report -DreportsPath=${basedir}${file.separator}target${file.separator}site  test
+mvn -Dbrowser="chrome" surefire-report:report -DreportsPath=${basedir}${file.separator}target${file.separator}site test
+mvn -Dbrowser="ie" surefire-report:report -DreportsPath=${basedir}${file.separator}target${file.separator}site test
+mvn -Dbrowser="phantomjs" surefire-report:report -DreportsPath=${basedir}${file.separator}target${file.separator}site test
 
 pause
